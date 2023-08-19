0603efe4927a6b77ae166b9bcc158cb3f8c27959
Geon <rndrjs123@naver.com>
2023-08-20 00:23:19 +0900

    refactor: split crawling page code into function

--- src/crawler.js
+++ src/crawler.js
@@ -2,23 +2,66 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 const { delay, iife } = require('./utils');
 
-const createUrlIterator = (host, urls) => {
-  let idx = 0;
-
-  return () => {
-    if (urls.length - 1 >= idx) {
-      idx++;
-      return host + urls[idx - 1];
-    }
-    return null;
-  };
-};
-
 const createDirs = (dirPath) => {
   const isExists = fs.existsSync(dirPath);
   if (!isExists) {
     fs.mkdirSync(dirPath, { recursive: true });
   }
+};
+
+const crawlingOnePage = async (
+  page,
+  url,
+  host,
+  outputRoot,
+  delayTime,
+  htmlPrefix
+) => {
+  console.log('Crawling: [Start] ', url);
+  await page.goto(url);
+
+  await delay(delayTime);
+
+  const htmlString = await page.evaluate(() => {
+    if (document.contentType !== 'text/html') {
+      return {
+        error: 'page is not text/html type',
+      };
+    }
+    return document.documentElement.innerHTML;
+  });
+  if (htmlString.error) {
+    console.error(`Crawling: [Error] ${url} ${htmlString.error}`);
+    return;
+  }
+
+  let path = url.replace(host, '');
+  if (path[path.length - 1] === '/') {
+    //if end with / (point to index.html)
+    path += 'index.html';
+  } else if (path.split('/').reverse()[0].split('.').length === 1) {
+    //if not specify file extension
+    path += '/index.html';
+  }
+
+  const outputPath = outputRoot + path;
+  const outputDir = iife(() => {
+    const dirs = `${outputPath}`.split('/');
+    dirs.splice(dirs.length - 1, 1);
+
+    return dirs.join('/');
+  });
+
+  createDirs(outputDir);
+
+  fs.writeFile(outputPath, htmlPrefix + htmlString, (e) => {
+    if (e)
+      console.error(
+        'Crawling: Cannot write crawler output file to webroot path\n',
+        e
+      );
+    else console.log(`Crawling: [OK] ${url}`);
+  });
 };
 
 const startCrawler = async (
@@ -30,63 +73,18 @@ const startCrawler = async (
   htmlPrefix
 ) => {
   console.log('Crawling: start');
+
   const browser = await puppeteer.launch({
     args: ['--disable-web-security'],
   });
   const page = await browser.newPage();
   await page.setUserAgent(userAgent);
 
-  const nextUrl = createUrlIterator(host, urls);
-
-  let current = null;
-  while ((current = nextUrl())) {
-    const url = current;
-    console.log('Crawling: [Start] ', url);
-    await page.goto(url);
-
-    await delay(delayTime);
-
-    const htmlString = await page.evaluate(() => {
-      if (document.contentType !== 'text/html') {
-        return {
-          error: 'page is not text/html type',
-        };
-      }
-      return document.documentElement.innerHTML;
-    });
-    if (htmlString.error) {
-      console.error(`Crawling: [Error] ${url} ${htmlString.error}`);
-      break;
-    }
-
-    let path = url.replace(host, '');
-    if (path[path.length - 1] === '/') {
-      //if end with / (point to index.html)
-      path += 'index.html';
-    } else if (path.split('/').reverse()[0].split('.').length === 1) {
-      //if not specify file extension
-      path += '/index.html';
-    }
-
-    const outputPath = outputRoot + path;
-    const outputDir = iife(() => {
-      const dirs = `${outputPath}`.split('/');
-      dirs.splice(dirs.length - 1, 1);
-
-      return dirs.join('/');
-    });
-
-    createDirs(outputDir);
-
-    fs.writeFile(outputPath, htmlPrefix + htmlString, (e) => {
-      if (e)
-        console.error(
-          'Crawling: Cannot write crawler output file to webroot path\n',
-          e
-        );
-      else console.log(`Crawling: [OK] ${url}`);
-    });
+  for (const pathname of urls) {
+    const url = host + pathname;
+    await crawlingOnePage(page, url, host, outputRoot, delayTime, htmlPrefix);
   }
+
   await browser.close();
 
   console.log('Crawling: end');
