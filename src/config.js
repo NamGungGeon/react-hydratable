c60a982103d9f54d76fd696c673f96968d483a6c
Geon <rndrjs123@naver.com>
2023-08-20 02:25:47 +0900

    feat: add config validation

--- src/config.js
+++ src/config.js
@@ -23,10 +23,36 @@ try {
     for (const configKey in config) {
       params[configKey] = config[configKey];
     }
+
+    //validation
+    if (!params.webroot || typeof params.webroot !== 'string') {
+      throw 'webroot must be non-empty string';
+    }
+    if (!params.host || typeof params.host !== 'string') {
+      throw 'host must be non-empty string';
+    }
+    if (params.port <= 0 && params.port !== '') {
+      throw 'port must be bigger than 0 or empty string';
+    }
+    if (!Array.isArray(params.crawlingUrls)) {
+      throw 'crawlingUrls must be string array';
+    }
+    if (typeof params.delay !== 'number') {
+      throw 'delay must be number';
+    }
+    if (typeof params.userAgent !== 'string') {
+      throw 'userAgent must be string';
+    }
+    if (typeof params.htmlPrefix !== 'string') {
+      throw 'htmlPrefix must be string';
+    }
+    if (typeof params.pageCount !== 'number' || params.pageCount <= 0) {
+      throw 'pageCount must be number and bigger than 0';
+    }
   }
 } catch (e) {
   console.error('Cannot read hydratable.config.json');
-  console.error(e);
+  throw e;
 }
 
 console.log('react-hydratable: Config is here\n', params);
